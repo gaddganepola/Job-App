7a4061c81442089349869ca2d8191d191eb639fd
gaddganepola <ganeypola@gmail.com>
2025-07-29 21:38:57 +0530

    job service created

---
+++ src/main/java/com/experts/JobApp/service/JobService.java
@@ -0,0 +1,13 @@
+package com.experts.JobApp.service;
+
+import com.experts.JobApp.model.JobPost;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobService {
+
+    public void addJob(JobPost jobPost) {
+
+    }
+
+}
