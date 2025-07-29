f69362bd07389f3d1c5827e13486b5c3ec62e5b5
gaddganepola <ganeypola@gmail.com>
2025-07-29 21:35:41 +0530

    add controller method to handle the form data

--- src/main/java/com/experts/JobApp/controller/JobController.java
+++ src/main/java/com/experts/JobApp/controller/JobController.java
@@ -1,5 +1,6 @@
 package com.experts.JobApp.controller;
 
+import com.experts.JobApp.model.JobPost;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,11 @@ public class JobController {
     @RequestMapping("addjob")
     public String addJob() {
         return "addjob";
+    }
+
+    @RequestMapping("handleForm")
+    public String handleForm(JobPost jobPost) {
+        return "success";
     }
 
 }
