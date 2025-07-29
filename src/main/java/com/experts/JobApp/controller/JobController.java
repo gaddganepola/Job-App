971fc7c29409a8a2247320cbc041f4c6311138fd
gaddganepola <ganeypola@gmail.com>
2025-07-29 19:35:20 +0530

    set up the property file and create controller to load the home page

---
+++ src/main/java/com/experts/JobApp/controller/JobController.java
@@ -0,0 +1,14 @@
+package com.experts.JobApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/", "home"})
+    public String home() {
+        return "home";
+    }
+
+}
