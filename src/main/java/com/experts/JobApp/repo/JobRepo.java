829d77a5b842a26fdccb24f254e5f6cbd0335b74
gaddganepola <ganeypola@gmail.com>
2025-07-29 21:38:44 +0530

    job repo created

---
+++ src/main/java/com/experts/JobApp/repo/JobRepo.java
@@ -0,0 +1,13 @@
+package com.experts.JobApp.repo;
+
+import com.experts.JobApp.model.JobPost;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JobRepo {
+
+    public void addJob(JobPost jobPost) {
+
+    }
+
+}
